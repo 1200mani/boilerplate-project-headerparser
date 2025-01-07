c430657b7c2f97e58ffe7d61fda6ee72538daf23
Mani Singh <ms23aal@herts.ac.uk>
2025-01-07 12:56:54 +0000

    Request Header Parser Microservice

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// index.js
-// where your node app starts
-
 // init project
 require('dotenv').config();
 var express = require('express');
@@ -22,6 +19,19 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// API endpoint for /api/whoami
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
